1dcbb07cd0077379cf68920b904ec2ddeaf2091d
gabrielsoares08 <gabrielgomessoares2007@gmail.com>
2026-09-02 22:39:13 -0300

    .

--- C++ learning/básico/constcpp/const.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main(){
-    //keyword que faz a variavel ser constante, ou seja, nao pode ser alterada
-    const int x = 5;
-    std::cout << x << std::endl;
-    return 0;
-}
